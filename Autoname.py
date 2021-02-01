a0ff05aaf30d6ff250b05c36ef347e60711d3677
Freepilot23 <freelancer.2101@gmail.com>
2021-02-01 22:22:19 +0300

    Add files via upload

---
+++ Autoname.py
@@ -0,0 +1,15 @@
+name = input ("Введите имя нового пользователя: ")
+surname == input ("Введите фамилиюнового пользователя: ")
+
+print ("Выберите компанию, в которую добавите пользователя \n 1 RNDS \n 2 Winwestor")
+company_name = int (input())
+if company_name == 1:
+    company = "RNDS"
+elif company_name == 2:
+    company = "Winwestor"
+else:
+    print ("Введите компанию")
+
+
+
+print (name, company, surname)
